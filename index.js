9377bb11f78d0b3755cfcfbbec89a820eca80f38
AzathothVoid <90100139+AzathothVoid@users.noreply.github.com>
2023-08-08 22:31:20 +0500

    practicing react js library

---
+++ index.js
@@ -0,0 +1,22 @@
+ReactDOM.render(
+  <div class="container block welcome-block grid grid--1x2">
+    <div class="welcome-block__header">
+      <h1 class="welcome-block__header__heading">Welcome, to React</h1>
+    </div>
+    <div class="welcome-block__content">
+      <ul>
+        <li>
+          <p>React is good</p>
+        </li>
+        <li>
+          <p>React is money</p>
+        </li>
+
+        <li>
+          <p>Learn fucking react</p>
+        </li>
+      </ul>
+    </div>
+  </div>,
+  document.getElementById("root")
+);
